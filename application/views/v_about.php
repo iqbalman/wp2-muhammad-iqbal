ef7d32656e638ebeaa99b8bd925aafa2a8a676d7
iqbalman <iqbalpratama150620@gmail.com>
2022-10-01 16:53:52 +0700

    Tugas 3

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+ <h1><?php echo $judul ?></h1>
+
+ <h4>Nama</h4>
+ <ul type="disc">
+ <li>Nama Depan : Muhammad</li>
+ <li>Nama Belakang : Iqbal</li>
+ </ul>
+ <br>
+ <h4>Alamat</h4>
+ <ul type="none">
+ <li> Jalan Imam Bonjol Gang Haji Salmah</li>
+ </ul>
+ 
+ <h4>Tempat Lahir</h4>
+ <ul type="none">
+ <li>Mandor</li>
+ </ul>
+ <h4>Olah Raga Favorit</h4>
+ <ul type="square">
+ <li>Voly</li>
+ <li>Badminton</li>
+ </ul>
+</section>
